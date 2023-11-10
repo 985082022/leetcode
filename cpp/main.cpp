0236bf22c06c2e42f616922c96cb8d9eeaf6d922
rhao <985082022@qq.com>
2023-11-10 16:22:35 +0800

    将leetcode题解代码上传

---
+++ cpp/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    int arr1[] = {1,2,3,4,5};
+    vector<int> a(2,10);
+    cout << a[0] ;
+    cout << endl ;
+    return 0;
+}
